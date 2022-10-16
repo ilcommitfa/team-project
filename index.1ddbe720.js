66f5685ed3422b88a5585c16df1fc5e1b2c4d754
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2022-10-16 19:57:56 +0000

    Deploying to gh-pages from @ ilcommitfa/team-project@fee216eea6ad2c8fbf44d2f0204607ecfcf93348 🚀

---
+++ index.1ddbe720.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[franchise-modal-open]"),closeModalBtn:document.querySelector("[franchise-modal-close]"),modal:document.querySelector("[franchise-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.1ddbe720.js.map
