b6d5b4427c3083e268a9ef821213d27e43c81183
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2022-10-17 06:12:59 +0000

    Deploying to gh-pages from @ ilcommitfa/team-project@fb35481da3f4fd1e953dd4d405fafb307ece21e9 🚀

--- index.1ddbe720.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[franchise-modal-open]"),closeModalBtn:document.querySelector("[franchise-modal-close]"),modal:document.querySelector("[franchise-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.1ddbe720.js.map
--- index.6024608b.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[franchise-modal-open]"),closeModalBtn:document.querySelector("[franchise-modal-close]"),modal:document.querySelector("[franchise-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.6024608b.js.map
